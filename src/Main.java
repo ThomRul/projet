d86784839e6181c8716096a83f5609e8b14d4118
thomas rulens <rulens.thomas@gmail.com>
2025-01-29 09:51:47 +0100

    tu travailles mal ! Cochon

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        printMessage("hello world");
+        printMessage("hello world", "pas bien");
     }
     // refacto hello in function
-    public static void printMessage(String message){
-        System.out.println(message);
+    public static void printMessage(String message, String pasMessage){
+        System.out.println(message + " " + pasMessage);
     }
 }
